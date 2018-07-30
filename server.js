bad7729cd77e3ce00272d1aa15cc2c127d6884bb
lewmoore <lewis.moore2707@gmail.com>
2018-07-30 17:12:57 +0100

    Add test for index route and rendering index view

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const app = express();
 let port = process.env.PORT || 8080;
+let bodyParser   = require('body-parser');
+app.use(bodyParser.json());
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
 
 app.listen(port)
 console.log("You're on localhost " + port);
