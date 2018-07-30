7033f3be6fe3be28bccffbb96d5cd64473a6001b
lewmoore <lewis.moore2707@gmail.com>
2018-07-30 17:09:11 +0100

    Add config for express app with host on 8080

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+let port = process.env.PORT || 8080;
+
+app.listen(port)
+console.log("You're on localhost " + port);
+
+module.exports = app
