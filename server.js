34b579c653d041473f927552ede6b9126364eea7
lewmoore <lewis.moore2707@gmail.com>
2018-07-30 17:48:58 +0100

    Add new array element on refresh

--- server.js
+++ server.js
@@ -5,15 +5,15 @@ let bodyParser   = require('body-parser');
 app.use(bodyParser.json());
 app.set('view engine', 'ejs');
 
-
 app.get('/', (req, res) => {
-  function GetValue() {
-    var myarray= new Array("item1","item2","item3");
+
+  function getValue() {
+    var myarray = new Array("item1","item2","item3","item4","item5");
     var random = myarray[Math.floor(Math.random() * myarray.length)];
     return random
   }
-  console.log(GetValue())
-  res.render('index', { random: GetValue() })
+  console.log(getValue)
+  res.render('index', { getValue: getValue() })
 })
 
 app.listen(port)
